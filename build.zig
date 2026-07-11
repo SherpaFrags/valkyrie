18a6e390e0efa783e80bc189834253582c17083d
SherpaFrags <absolute_s0lver@outlook.com>
2026-07-11 15:05:17 -0400

    restarting and changing focus. zig implementation example code debugging

--- build.zig
+++ build.zig
@@ -41,7 +41,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
     });
 
-    // Here we define an executable. An executable needs to have a root module
+    // Here we define an exec:utable. An executable needs to have a root module
     // which needs to expose a `main` function. While we could add a main function
     // to the module defined above, it's sometimes preferable to split business
     // logic and the CLI into two separate modules.
