8ab9dc23d82815be880fc8244ec1ee48ed5a7773
SherpaFrags <absolute_s0lver@outlook.com>
2026-07-11 14:59:01 -0400

    restarting and changing focus. zig implementation example code debugging

---
+++ .zig-cache/o/d532c89cf1d19d9cd014491975aec4dc/dependencies.zig
@@ -0,0 +1,76 @@
+pub const packages = struct {
+    pub const @"N-V-__8AAAHFTwBfBg00dnFXcYkNbjC8BSyi1CDcrnh12ypL" = struct {
+        pub const available = false;
+    };
+    pub const @"N-V-__8AAHNaAgD6kjK3HX6lAf89chPqS5zIm0tLpvBQLOtX" = struct {
+        pub const available = false;
+    };
+    pub const @"N-V-__8AAPwQuguI8Jsun6Omu2HHb9EADaaP8Yn2TxYplJtC" = struct {
+        pub const available = false;
+    };
+    pub const @"SDL-2.32.10-JToi37G3EgG0eraPos4vhCwC6vVgyB1FYf1SxxhpBr_Y" = struct {
+        pub const available = false;
+    };
+    pub const @"dvui-0.5.0-dev-AQFJmWfx-wBObpJ1cR37uegD8XexNPqnhOI41jPAOYcY" = struct {
+        pub const build_root = "/home/hellbents/valkyrie/zig-pkg/dvui-0.5.0-dev-AQFJmWfx-wBObpJ1cR37uegD8XexNPqnhOI41jPAOYcY";
+        pub const build_zig = @import("dvui-0.5.0-dev-AQFJmWfx-wBObpJ1cR37uegD8XexNPqnhOI41jPAOYcY");
+        pub const deps: []const struct { []const u8, []const u8 } = &.{
+            .{ "sdl", "SDL-2.32.10-JToi37G3EgG0eraPos4vhCwC6vVgyB1FYf1SxxhpBr_Y" },
+            .{ "sdl3", "sdl-0.4.2+3.4.4-SDL--tB1pQFLgwvCakiiOSoZIuVaz6a6-_gOZC66AGRD" },
+            .{ "freetype", "freetype-2.14.1-alzUkceCqgDRZIP8PnxnNCd8jwfbwY0k1VhlTV2O5Ivo" },
+            .{ "raylib", "raylib-6.0.0-whq8uOmvLgUktXC94fZkRLK2dQA75X2pMwZYj2X3EZl8" },
+            .{ "wio", "wio-0.0.0-8xHrrwXRBwCM2DKFfG5xE3RkA3u5giXBKP9nIxtfx_EF" },
+            .{ "opengl", "opengl-0.0.0-_Vp8-EjsKgDhnkUWeb8Nmr2oEdvsCsDq9bUgmcNi0SuQ" },
+            .{ "raygui", "N-V-__8AAAHFTwBfBg00dnFXcYkNbjC8BSyi1CDcrnh12ypL" },
+            .{ "win32", "zigwin32-25.0.28-preview-6BfVhYKN5AOvXPVJq4bGZ7C5dM3PbUra4xhKAKOFJGcF" },
+            .{ "svg2tvg", "svg2tvg-0.0.0-HpMZuaP8BQALdwSolfst0dPGUUn2ptqgfSxVs8RoQJ6K" },
+            .{ "accesskit", "N-V-__8AAPwQuguI8Jsun6Omu2HHb9EADaaP8Yn2TxYplJtC" },
+            .{ "raylib_zig", "raylib_zig-5.6.0-dev-KE8REKNmBQDek2Sz27ULioxYpY9IYR0K0CeIC-iJRLCI" },
+            .{ "zglfw", "zglfw-0.10.0-dev-zgVDNC64IQCeL-_eVOA27Wa50WVitrJswOZVvgcXmIsy" },
+            .{ "tree_sitter", "tree_sitter-0.27.0-Tw2sR72eCwCRZKEZND6TJIgdWq9ZnPV9g_syCHLyNEGe" },
+            .{ "tree_sitter_json", "N-V-__8AAHNaAgD6kjK3HX6lAf89chPqS5zIm0tLpvBQLOtX" },
+            .{ "tree_sitter_zig", "tree_sitter_zig-1.1.2-YNf2GAt8XACdbaWHHiNMMfw--bLN1Av1rMsnq6bokMdL" },
+            .{ "zgl", "zgl-1.1.0-p_NpAJNECgCzUGx0aF5KFsmD5VOwfw8LaOsN0fcoU2bi" },
+        };
+    };
+    pub const @"freetype-2.14.1-alzUkceCqgDRZIP8PnxnNCd8jwfbwY0k1VhlTV2O5Ivo" = struct {
+        pub const available = false;
+    };
+    pub const @"opengl-0.0.0-_Vp8-EjsKgDhnkUWeb8Nmr2oEdvsCsDq9bUgmcNi0SuQ" = struct {
+        pub const available = false;
+    };
+    pub const @"raylib-6.0.0-whq8uOmvLgUktXC94fZkRLK2dQA75X2pMwZYj2X3EZl8" = struct {
+        pub const available = false;
+    };
+    pub const @"raylib_zig-5.6.0-dev-KE8REKNmBQDek2Sz27ULioxYpY9IYR0K0CeIC-iJRLCI" = struct {
+        pub const available = false;
+    };
+    pub const @"sdl-0.4.2+3.4.4-SDL--tB1pQFLgwvCakiiOSoZIuVaz6a6-_gOZC66AGRD" = struct {
+        pub const available = false;
+    };
+    pub const @"svg2tvg-0.0.0-HpMZuaP8BQALdwSolfst0dPGUUn2ptqgfSxVs8RoQJ6K" = struct {
+        pub const available = false;
+    };
+    pub const @"tree_sitter-0.27.0-Tw2sR72eCwCRZKEZND6TJIgdWq9ZnPV9g_syCHLyNEGe" = struct {
+        pub const available = false;
+    };
+    pub const @"tree_sitter_zig-1.1.2-YNf2GAt8XACdbaWHHiNMMfw--bLN1Av1rMsnq6bokMdL" = struct {
+        pub const available = false;
+    };
+    pub const @"wio-0.0.0-8xHrrwXRBwCM2DKFfG5xE3RkA3u5giXBKP9nIxtfx_EF" = struct {
+        pub const available = false;
+    };
+    pub const @"zgl-1.1.0-p_NpAJNECgCzUGx0aF5KFsmD5VOwfw8LaOsN0fcoU2bi" = struct {
+        pub const available = false;
+    };
+    pub const @"zglfw-0.10.0-dev-zgVDNC64IQCeL-_eVOA27Wa50WVitrJswOZVvgcXmIsy" = struct {
+        pub const available = false;
+    };
+    pub const @"zigwin32-25.0.28-preview-6BfVhYKN5AOvXPVJq4bGZ7C5dM3PbUra4xhKAKOFJGcF" = struct {
+        pub const available = false;
+    };
+};
+
+pub const root_deps: []const struct { []const u8, []const u8 } = &.{
+    .{ "dvui", "dvui-0.5.0-dev-AQFJmWfx-wBObpJ1cR37uegD8XexNPqnhOI41jPAOYcY" },
+};
